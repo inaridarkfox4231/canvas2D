0a0df4e962a24b9cfa866e69fb1af6f9bf0b57b5
INARI_DARKFOX <39549290+inaridarkfox4231@users.noreply.github.com>
2024-10-07 15:58:45 +0900

    追記

--- src/script.js
+++ src/script.js
@@ -1,24 +1,41 @@
-let gr;
-let ratio;
-
-function setup(){
-  createCanvas(windowWidth, windowHeight);
-  pixelDensity(1);
-  gr = createGraphics(400,400);
-
-  ratio = max(1,windowWidth/window.screen.width);
+let offsetY = 5;
+let offsetVelocity=0;
+function setup() {
+	createCanvas(windowWidth,windowHeight);
+	//pixelDensity(1);
+	const IR = new foxIA.Inspector(this.canvas);
+	IR.add("wheel",(e)=>{
+		offsetVelocity -= e.deltaY*0.05;
+	});
 }
-
 function draw(){
-  clear();
-  image(gr, width/2-ratio*gr.width/2, height/2-ratio*gr.height/2, ratio*gr.width,ratio*gr.height,0,0,gr.width,gr.height);
-
-  gr.background("navy");
-  gr.noStroke();
-  gr.textAlign(LEFT,TOP);
-  gr.textSize(18);
-  gr.text("Hello, world.", 5, 5);
-  gr.text("windowWidth:" + windowWidth, 5, 35);
-  gr.fill(255);
-  gr.circle(200+150*cos(frameCount*TAU/120),200+150*sin(frameCount*TAU/120),10);
+	offsetY = constrain(offsetY + offsetVelocity, -1000, 5);
+	offsetVelocity *= 0.85;
+	if(abs(offsetVelocity)<0.001)offsetVelocity=0;
+	background(255);
+	const cvs=document.getElementById("defaultCanvas0");
+	stl=window.getComputedStyle(cvs);
+	textSize(15);
+	textAlign(LEFT,TOP);
+	let a=5;
+	let b=offsetY;
+	text("width:"+width,a,b);
+	text("windowWidth:"+windowWidth,a,b+=30);
+	text("window.innerWidth:"+window.innerWidth,a,b+=30);
+	text("window.screen.width:"+window.screen.width,a,b+=30);
+	text("height:"+height,a,b+=30);
+	text("windowHeight:"+windowHeight,a,b+=30);
+	text("window.innerHeight:"+window.innerHeight,a,b+=30);
+	text("window.screen.height:"+window.screen.height,a,b+=30);
+	text("devicePixelRatio:"+window.devicePixelRatio,a,b+=30);
+	text(stl.getPropertyValue("width"),a,b+=30);
+	text(stl.getPropertyValue("height"),a,b+=30);
+  text("document.documentElement.clientWidth",a,b+=30);
+	text(document.documentElement.clientWidth,a,b+=30);
+  text("document.documentElement.clientHeight",a,b+=30);
+	text(document.documentElement.clientHeight,a,b+=30);
+  text("document.body.clientWidth",a,b+=30);
+	text(document.body.clientWidth,a,b+=30);
+  text("document.body.clientHeight",a,b+=30);
+	text(document.body.clientHeight,a,b+=30);
 }
